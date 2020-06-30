8a3f970a3b83cb1279643e244af06a81fe1fa622
Ivica Bogosavljevic <ivica@johnysswlab.com>
2020-06-30 08:31:56 +0200

    Update files

--- 2020-07-branches/counting.cpp
+++ 2020-07-branches/counting.cpp
@@ -4,7 +4,11 @@
 #include "argparse.h"
 
 
-int count_bigger_than_limit(int* array, int n, int limit) {
+typedef int count_bigger_than_limit_t(int* array, int n, int limit);
+
+
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_regular(int* array, int n, int limit) {
     int limit_cnt = 0;
     for (int i = 0; i < n; i++) {
         if (array[i] > limit) {
@@ -15,6 +19,7 @@ int count_bigger_than_limit(int* array, int n, int limit) {
     return limit_cnt;
 }
 
+__attribute__ ((optimize(FUNC_OPT)))
 int count_bigger_than_limit_branchless(int* array, int n, int limit) {
     int limit_cnt[] = { 0, 0 };
     for (int i = 0; i < n; i++) {
@@ -23,29 +28,188 @@ int count_bigger_than_limit_branchless(int* array, int n, int limit) {
     return limit_cnt[1];
 }
 
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_arithmetic(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    for (int i = 0; i < n; i++) {
+        limit_cnt += (array[i] > limit);
+    }
+    return limit_cnt;
+}
 
-int main(int argc, char** argv) {
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_cmove(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    int new_limit_cnt;
+    for (int i = 0; i < n; i++) {
+        new_limit_cnt = limit_cnt + 1;
+        __asm__ (
+            "cmp %[array_i], %[limit];"
+            "cmovbe %[new_limit_cnt], %[limit_cnt];"
+	        : [limit_cnt] "+&r"(limit_cnt)
+            : [array_i] "g"(array[i]), [new_limit_cnt] "g"(new_limit_cnt), [limit] "r"(limit)
+            : "cc"
+	    );
+    }
+    return limit_cnt;
+}
+
+#define likely(x)       __builtin_expect(!!(x),1)
+#define unlikely(x)     __builtin_expect(!!(x),0)
+
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_likely(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (likely(array[i] > limit)) {
+            limit_cnt++;
+        }
+    }
+
+    return limit_cnt;
+}
+
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_unlikely(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (unlikely(array[i] > limit)) {
+            limit_cnt++;
+        }
+    }
+
+    return limit_cnt;
+}
+
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_joint_simple(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    for (int i = 0; i < n/2; i+=2) {
+        if (array[i] > limit && array[i + 1] > limit) {
+            limit_cnt++;
+        }
+    }
+    return limit_cnt;
+}
+
+__attribute__ ((optimize(FUNC_OPT)))
+int count_bigger_than_limit_joint_arithmetic(int* array, int n, int limit) {
+    int limit_cnt = 0;
+    for (int i = 0; i < n/2; i+=2) {
+        if (array[i] > limit & array[i + 1] > limit) {
+            limit_cnt++;
+        }
+    }
+    return limit_cnt;
+}
+
+using namespace argparse;
+
+int main(int argc, const char** argv) {
+    count_bigger_than_limit_t* count_func = nullptr;
+    std::string count_func_name;
+
     int searches = 500;
     int arr_len = 10000000;
+    float condition = 0.5;
+
+    std::vector<std::pair<std::string, count_bigger_than_limit_t*>> count_funcs = {
+         { "regular", count_bigger_than_limit_regular },
+         { "cmov", count_bigger_than_limit_cmove},
+         { "arithmetic", count_bigger_than_limit_arithmetic },
+         { "branchless", count_bigger_than_limit_branchless },
+         { "likely", count_bigger_than_limit_likely },
+         { "unlikely", count_bigger_than_limit_unlikely },
+         { "joint_simple", count_bigger_than_limit_joint_simple },
+         { "joint_arithmetic", count_bigger_than_limit_joint_arithmetic },
+    };
+    ArgumentParser parser("test123", "123");
+
+    parser.add_argument("-t", "--test", "Name of the test to run", true);
+    parser.add_argument("-s", "--searches", "Number of searches to perform", false);
+    parser.add_argument("-l", "--length", "Array length", false);
+    parser.add_argument("-c", "--condition", "Probability of condition being true (0-1)", false);
+    
+    auto err = parser.parse(argc, argv);
+    if (err) {
+        std::cout << err << std::endl;
+        return -1;
+    }
+    
+    if (parser.exists("t")) {
+        std::string test_name = parser.get<std::string>("t");
+        std::cout << "test name : " << test_name << std::endl;
+        for (const auto& param: count_funcs) {
+            if (param.first == test_name) {
+                count_func_name = std::move(test_name);
+                count_func = param.second;
+                break;
+            }
+        }
+        if (count_func == nullptr) {
+            std::cout << "Unknown value for --test\n";
+            return -1;
+        }   
+    } else {
+        std::cout << "Parameter --test needed. Possible values for paramter: " << "\n";
+        for (const auto& param: count_funcs) {
+            std::cout << "--test " << param.first << "\n";
+        }
+        return -1;
+    }
+
+    if (parser.exists("s")) {
+        int searches_cmdl = parser.get<int>("s");
+        if (searches_cmdl > 0 && searches_cmdl <= 10000000) {
+            searches = searches_cmdl;
+        }
+    }
+
+    if (parser.exists("l")) {
+        int arr_len_cmdl = parser.get<int>("l");
+        if (arr_len_cmdl >= 1000 && arr_len_cmdl <= 100000000) {
+            arr_len = arr_len_cmdl;
+        }
+    }
+
+    if (parser.exists("c")) {
+        float condition_cmdl = parser.get<float>("c");
+        if (condition_cmdl >= 0.0 && condition_cmdl <= 1.0) {
+            condition = condition_cmdl;
+        }
+    }
+
     int res;
     std::vector<int> arr = create_random_array<int>(arr_len, 0, arr_len);
 
+    std::cout << "Running test: " << count_func_name << ", on array length " << arr_len << 
+                 ", num of searches " << searches << std::endl; 
+    std::cout << "Conditon probability: " << 100.0 * condition << "%" << std::endl;
     {
-        measure_time m("searches random");
+        measure_time m("Algorithm");
         for (int i = 0; i < searches; i++) {
-            res = count_bigger_than_limit(arr.data(), arr_len, arr_len / 2);
+            res = count_func(arr.data(), arr_len, arr_len * condition);
+        }
+    }
+
+    std::cout << (res * 100.0) / arr_len << "% of data fulfills the criterium\n";
+/*
+    {
+        measure_time m("Condition always happens");
+        for (int i = 0; i < searches; i++) {
+            res = count_func(arr.data(), arr_len, arr_len);
             escape(&res);
         }
     }
-    std::cout << "RES = " << res;
+
     {
-        measure_time m("searches unifrom");
+        measure_time m("Condition never happens");
         for (int i = 0; i < searches; i++) {
-            res = count_bigger_than_limit(arr.data(), arr_len, arr_len);
+            res = count_func(arr.data(), arr_len, arr_len);
             escape(&res);
         }
     }
-    std::cout << "RES = " << res;
+*/
 
     return 0;
 }
--- 2020-07-branches/measure_time.h
+++ 2020-07-branches/measure_time.h
@@ -48,18 +48,20 @@ private:
 template <typename T>
 measure_time_database<T>* measure_time_database<T>::my_instance;
 
-#define array_length(x) (sizeof(x) / sizeof(x[0]))
-
 class measure_time {
 public:
 
     measure_time(const std::string& message) : message_(message) {
+
+        int events[events_length];
+
+        load_events(events, events_length);
         
         std::cout << "Starting measurement for \"" << message_ << "\"\n";
 
         start_time = std::chrono::high_resolution_clock::now();
         int ret_val = PAPI_start_counters(events, events_length);
-        papi_valid = ret_val == PAPI_OK;        
+        papi_valid = ret_val == PAPI_OK;
         asm volatile("": : :"memory");
     }
 
@@ -67,30 +69,104 @@ public:
         asm volatile("": : :"memory");
 
         auto end_time = std::chrono::high_resolution_clock::now();
-        long_long values[array_length(events)];
+        long_long values[events_length];
 
         if (papi_valid) {
-            papi_valid = PAPI_stop_counters(values, events_length) == PAPI_OK; 
-        } else {
-            std::cout << "PAPI not valid\n";
+            papi_valid = PAPI_stop_counters(values, events_length) == PAPI_OK;
+            if (!papi_valid) {
+                std::cout << "PAPI not valid\n";
+            } 
         }
-
         
         std::chrono::milliseconds time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
         std::cout << "\"" << message_ << "\" took " << time /std::chrono::milliseconds(1) << "ms to run.\n";
         if (papi_valid) {
-            std::cout << "\t" << "L1 Cache missess: " << values[0] << "\n";
-            std::cout << "\t" << "L1 Cache accesses: " << values[1] << "\n";
-            std::cout << "\t" << "L1 Cache miss rate: " << values[0] * 100.0 / values[1] << "%\n";
+            std::cout << "\t" << "Total instructions: " << values[0] << std::endl;
+            std::cout << "\t" << "Total cycles: " << values[1] << std::endl; 
+            std::cout << "\t" << "Instruction per cycle: " << values[0] / (float) values[1] << std::endl;
+            print_dcache(values);
+            print_branches(values);
         }
         measure_time_database<std::chrono::milliseconds>::get_instance()->set_result(message_, time);
     }
+
+    static void initialize(int* events) {
+        static bool initialized = false;
+
+        if (initialized) {
+            return;
+        }
+
+        initialized = true;
+
+        char * measure_string_str = std::getenv("MEASURE_FLAGS");
+        events[0] = PAPI_TOT_INS;
+        events[1] = PAPI_TOT_CYC;
+        events[2] = PAPI_L1_DCM;
+        events[3] = PAPI_L1_DCA;
+        if (measure_string_str) { 
+            std::string measure_flags(measure_string_str);
+            if (measure_flags == "DCACHE") {
+                get_what_to_measure() = what_to_measure::DCACHE;
+                events[2] = PAPI_L1_DCM;
+                events[3] = PAPI_L1_DCA;
+            } else if (measure_flags == "BRANCH") {
+                get_what_to_measure() = what_to_measure::BRANCHES;
+                events[2] = PAPI_BR_MSP;
+                events[3] = PAPI_BR_TKN;
+            }
+        }
+    }
+
+    void print_dcache(long_long values[]) {
+        if (get_what_to_measure() != what_to_measure::DCACHE) {
+            return;
+        }
+
+        std::cout << "\t" << "L1 Cache missess: " << values[2] << "\n";
+        std::cout << "\t" << "L1 Cache accesses: " << values[3] << "\n";
+        std::cout << "\t" << "L1 Cache miss rate: " << values[2] * 100.0 / values[3] << "%\n";
+    }
+
+    void print_branches(long_long values[]) {
+        if (get_what_to_measure() != what_to_measure::BRANCHES) {
+            return;
+        }
+
+        std::cout << "\t" << "Branches missprediced : " << values[2] << "\n";
+        std::cout << "\t" << "Total branches: " << values[3] << "\n";
+        std::cout << "\t" << "Branch missprediction rate: " << (values[2] * 100.0) / values[3] << "%\n";
+    }
+
+
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
     std::string message_;
     bool papi_valid;
 
-    static constexpr int events_length = 2;
+    static constexpr int events_length = 4;
 
-    int events[events_length] = {PAPI_L1_DCM, PAPI_L1_DCA };
+    enum what_to_measure {
+        DEFAULT,
+        DCACHE,
+        BRANCHES,
+    };
+
+    static what_to_measure& get_what_to_measure() {
+        static what_to_measure wtm;
+        return wtm; 
+    }
+
+    static int* get_events_array(){
+        static int events[events_length];
+        initialize(events);
+        return events;
+    }
+
+    void load_events(int* events_array, int len) {
+        int* static_events = get_events_array();
+        for (int i = 0; i < len; i++) {
+            events_array[i] = static_events[i];
+        }
+    }
 };
