2ef75af880ceef3964f5e238c49c04b4fe3c69f2
Ivica Bogosavljevic <ivica@johnysswlab.com>
2020-08-08 15:28:21 +0200

    Simplify allocator

--- 2020-08-memoryaccess/custom_allocator.h
+++ 2020-08-memoryaccess/custom_allocator.h
@@ -75,92 +75,48 @@ public:
 
 #include <sys/mman.h>
 
-template <typename _Tp, int gap>
-class zone_allocator
-{
+
+template <class T, int gap>
+class zone_allocator {
 private:
-    _Tp* my_memory;
-    _Tp* last_location;
+    T* my_memory;
+    T* last_location;
     int free_block_index;
     int start_index;
-    static constexpr int mem_size = 1000*1024*1024;
+    static constexpr int mem_size = 390*1024*1024;
 public:
-    typedef size_t size_type;
-    typedef ptrdiff_t difference_type;
-    typedef _Tp* pointer;
-    typedef const _Tp* const_pointer;
-    typedef _Tp& reference;
-    typedef const _Tp& const_reference;
-    typedef _Tp value_type;
-
-    template <typename _Tp1>
-    struct rebind {
-        typedef zone_allocator<_Tp1, gap> other;
-    };
-
-    zone_allocator()
-    {
+    typedef T value_type;
+    zone_allocator() noexcept {
         char * p = reinterpret_cast<char*>(mmap(0, mem_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0));
-        my_memory = reinterpret_cast<_Tp*>(p);
-        last_location = reinterpret_cast<_Tp*>(p + mem_size);
+        my_memory = reinterpret_cast<T*>(p);
+        last_location = reinterpret_cast<T*>(p + mem_size);
         free_block_index = 0;
         start_index = 0;
-    }
 
-    zone_allocator(const zone_allocator&)
-    {
+        std::cout << "Type size = " << sizeof(T) << std::endl;
     }
-
-    template <typename _Tp1, int gap1>
-    zone_allocator(const zone_allocator<_Tp1, gap1>&)
-    {
-    }
-
-    ~zone_allocator()
-    {
+    ~zone_allocator() {
         munmap(my_memory, mem_size);
     }
 
-    pointer address(reference __x) const
-    {
-        return std::__addressof(__x);
-    }
+    template<class U>
+    struct rebind { using other = zone_allocator<U, gap>; };    
 
-    const_pointer address(const_reference __x) const
-    {
-        return std::__addressof(__x);
-    }
-
-    pointer allocate(size_type __n, const void* = 0)
-    {
-        assert(__n == 1);
-        pointer result = &my_memory[free_block_index];
-        free_block_index += __n + gap;
+    template <class U> zone_allocator (const custom_allocator<U>&) noexcept {}
+    T* allocate (std::size_t n) { 
+        assert(n == 1);
+        T* result = &my_memory[free_block_index];
+        free_block_index += n + gap;
 
         if (&my_memory[free_block_index] >= last_location) {
             start_index++;
             free_block_index = start_index;
         }
         return result;
-    }
 
-    void deallocate(pointer __p, size_type __n)
-    {
-        // We deallocate everything when destroyed
     }
-
-    size_type max_size() const
-    {
-        return size_t(-1) / sizeof(_Tp);
-    }
-
-    void construct(pointer __p, const _Tp& __val)
-    {
-        ::new ((void*)__p) _Tp(__val);
-    }
-
-    void destroy(pointer __p)
-    {
-        __p->~_Tp();
-    }
+    void deallocate (T* p, std::size_t n) { 
+        // noop
+     }
 };
+
