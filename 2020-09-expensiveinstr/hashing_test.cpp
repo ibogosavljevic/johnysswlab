ef80809fe22b26d48242b5c7fb1ae9fffa7860c6
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2020-09-12 09:11:53 +0200

    Reuse size parameter for the size of hash map

--- 2020-09-expensiveinstr/hashing_test.cpp
+++ 2020-09-expensiveinstr/hashing_test.cpp
@@ -12,7 +12,7 @@ enum hash_type_e { SIMPLE, SHIFT };
 bool parse_args(int argc,
                 const char* argv[],
                 hash_type_e& out_hash_type,
-                size_t& out_element_size) {
+                size_t& out_size) {
     ArgumentParser parser("test123", "123");
 
     parser.add_argument("-h", "--hash",
@@ -44,11 +44,11 @@ bool parse_args(int argc,
         std::string size = parser.get<std::string>("s");
         std::cout << "Size of the hash map : " << size << std::endl;
         if (size == "s") {
-            out_element_size = 16 * 1024;
+            out_size = 16 * 1024;
         } else if (size == "m") {
-            out_element_size = 8 * 1024 * 1024;
+            out_size = 8 * 1024 * 1024;
         } else if (size == "l") {
-            out_element_size = 64 * 1024 * 1024;
+            out_size = 64 * 1024 * 1024;
         } else {
             std::cout << "Unknown value for --size\n";
             return false;
@@ -61,31 +61,50 @@ bool parse_args(int argc,
 }
 
 template <typename T, typename Q>
-void run_test(int size) {
+size_t run_test(int size) {
     int arr_len = size;
     std::vector<int> v = create_random_array<int>(arr_len, 0, arr_len);
     hash_map<Q, T> my_map(arr_len);
+    size_t found = 0;
+
+    size_t iterations = 64 * 1024 * 1024 / size;
+    iterations = iterations == 0 ? 1 : iterations;
 
     int count = 0.7 * size;
 
-    for (size_t i = 0; i < count; i++) {
-        my_map.insert(Q(v[i]));
+    for (int j = 0; j < iterations; j++) {
+        for (size_t i = 0; i < count; i++) {
+            my_map.insert(Q(v[i]));
+        }
+
+        for (size_t i = 0.3 * size; i < size; i++) {
+            my_map.remove(v[i]);
+        }
+
+        for (size_t i = 0; i < size; i++) {
+            found += my_map.find(v[i]);
+        }
     }
+
+    return found;
 }
 
 int main(int argc, const char* argv[]) {
     hash_type_e hash_type;
     size_t size = 0;
+    size_t found;
 
     if (!parse_args(argc, argv, hash_type, size)) {
         return false;
     }
 
     if (hash_type == hash_type_e::SIMPLE) {
-        run_test<simple_hasher, int>(size);
+        found = run_test<simple_hasher, int>(size);
     } else if (hash_type == hash_type_e::SHIFT) {
-        run_test<shift_hasher, int>(size);
+        found = run_test<shift_hasher, int>(size);
     }
+
+    std::cout << "Found " << found << std::endl;
 
     return 0;
 }
