0d524f2a380203afdc05f00cd112cf9eef17af82
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2020-09-29 14:40:07 +0200

    Cachebatchprocessing: Initial commit

---
+++ 2020-10-cachebatchprocessing/hash_map.h
@@ -0,0 +1,292 @@
+#include <stdlib.h>
+#include <cassert>
+#include <cstring>
+#include <functional>
+#include <iostream>
+
+namespace jsl {
+
+struct simple_hasher {
+    size_t m_limit;
+    void set_limit(size_t limit) { m_limit = limit; }
+    size_t limit_input(size_t val) { return val % m_limit; }
+    size_t limit_input(size_t val, size_t limit) { return val % m_limit; }
+};
+
+struct shift_hasher {
+    size_t m_limit;
+    void set_limit(size_t limit) { m_limit = limit - 1; }
+    size_t limit_input(size_t val) { return (val & m_limit); }
+    size_t limit_input(size_t val, size_t limit) { return (val & (limit - 1)); }
+};
+
+template <typename T, typename F = shift_hasher>
+class hash_map {
+   public:
+    hash_map(size_t capacity)
+        : m_log_size(next_pow2_log(capacity)),
+          m_size(1 << m_log_size),
+          m_used(0),
+          m_used_and_deleted(0),
+          m_rehashing_threshhold(m_size * 0.7) {
+        m_limiter.set_limit(m_size);
+        int result = posix_memalign((void**)&m_values, 64, m_size * sizeof(T));
+        if (result != 0) {
+            throw std::bad_alloc();
+        }
+        m_value_used = (char*)malloc(m_size);
+
+        std::memset(m_values, 0, m_size * sizeof(T));
+        std::memset(m_value_used, 0, m_size);
+    }
+
+    ~hash_map() {
+        free(m_value_used);
+        free(m_values);
+    }
+
+    hash_map() : hash_map(DEFAULT_HASH_SIZE) {}
+
+    template <bool calculate_hash = true>
+    bool find(const T& val, size_t hash = 0) {
+        size_t entry_start;
+
+        if (calculate_hash) {
+            entry_start = m_limiter.limit_input(m_hasher(val));
+        } else {
+            entry_start = hash;
+        }
+
+        size_t entry_current = entry_start;
+
+        do {
+            if (m_value_used[entry_current] == BUCKET_USED) {
+                if (*get(m_values, entry_current) == val) {
+                    return true;
+                } else {
+                    entry_current++;
+                }
+            } else if (m_value_used[entry_current] == BUCKET_DELETED) {
+                return entry_current++;
+            } else {
+                return false;
+            }
+
+            if (entry_current == m_size) {
+                entry_current = 0;
+            }
+        } while (entry_current != entry_start);
+
+        return false;
+    }
+
+    template <int parallel_searches = 2>
+    std::vector<bool> find(const std::vector<T>& in_values) {
+        std::vector<bool> result(in_values.size(), false);
+        std::array<size_t, parallel_searches> hashes;
+
+        // Round the length to lower four
+        int len = (in_values.size() / parallel_searches) * parallel_searches;
+
+        for (int i = 0; i < len; i += parallel_searches) {
+            for (int j = 0; j < parallel_searches; j++) {
+                size_t hash = m_limiter.limit_input(m_hasher(in_values[i + j]));
+                //__builtin_prefetch(&m_values[hash]);
+                //__builtin_prefetch(&m_value_used[hash]);
+                hashes[j] = hash;
+            }
+
+            for (int j = 0; j < parallel_searches; j++) {
+                size_t index = i + j;
+                result[index] = find<false>(in_values[index], hashes[j]);
+            }
+        }
+
+        return result;
+    }
+
+    bool insert(const T& val) {
+        size_t entry_start = m_limiter.limit_input(m_hasher(val));
+        size_t entry_current = entry_start;
+
+        do {
+            if (m_value_used[entry_current] == BUCKET_USED) {
+                if (*get(m_values, entry_current) == val) {
+                    return false;
+                } else {
+                    entry_current++;
+                    if (entry_current == m_size) {
+                        entry_current = 0;
+                    }
+                }
+            } else {
+                break;
+            }
+
+        } while (entry_current != entry_start);
+
+        if (m_value_used[entry_current] == BUCKET_FREE) {
+            m_used++;
+            m_used_and_deleted++;
+        } else {
+            // DELETED
+            m_used++;
+        }
+
+        // DELETED or FREE bucket can be used
+        ::new (get(m_values, entry_current)) T(val);
+        m_value_used[entry_current] = BUCKET_USED;
+
+        if (m_used_and_deleted > m_rehashing_threshhold) {
+            if (m_used > (0.8 * m_rehashing_threshhold)) {
+                grow_and_rehash(m_log_size + 1);
+            } else {
+                grow_and_rehash(m_log_size);
+            }
+        }
+
+        return true;
+    }
+
+    bool remove(const T& val) {
+        size_t entry_start = m_limiter.limit_input(m_hasher(val));
+        size_t entry_current = entry_start;
+
+        do {
+            if (m_value_used[entry_current] == BUCKET_USED) {
+                T* v = get(m_values, entry_current);
+                if (*v == val) {
+                    v->~T();
+                    m_value_used[entry_current] = BUCKET_DELETED;
+                    m_used--;
+                    return true;
+                } else {
+                    entry_current++;
+                }
+            } else if (m_value_used[entry_current] == BUCKET_DELETED)
+                entry_current++;
+            else {
+                return false;
+            }
+
+            if (entry_current == m_size) {
+                entry_current = 0;
+            }
+        } while (entry_current != entry_start);
+
+        return false;
+    }
+
+    void dump() {
+        std::cout << "Hash map size = " << m_size
+                  << ", log size = " << m_log_size << std::endl;
+        std::cout << "Hash map, used = " << m_used
+                  << ", used and deleted = " << m_used_and_deleted << std::endl;
+        std::cout << "Hash map rehashing threshhold = "
+                  << m_rehashing_threshhold << std::endl;
+
+        for (int i = 0; i < m_size; i++) {
+            std::cout << "Entry " << i << ": ";
+            if (m_value_used[i] == BUCKET_FREE) {
+                std::cout << "FREE";
+            } else if (m_value_used[i] == BUCKET_DELETED) {
+                std::cout << "DELETED";
+            } else {
+                T* v = get(m_values, i);
+                size_t original_entry = m_limiter.limit_input(m_hasher(*v));
+                std::cout << "USED, original entry " << original_entry
+                          << ", value = " << *v;
+            }
+            std::cout << std::endl;
+        }
+    }
+
+   private:
+    static constexpr char BUCKET_FREE = 0;
+    static constexpr char BUCKET_USED = 1;
+    static constexpr char BUCKET_DELETED = 2;
+    static constexpr size_t DEFAULT_HASH_SIZE = 16;
+
+    char* m_values;
+    char* m_value_used;
+
+    size_t m_log_size;
+    size_t m_size;
+    size_t m_used;
+    size_t m_used_and_deleted;
+    size_t m_rehashing_threshhold;
+
+    std::hash<T> m_hasher;
+
+    F m_limiter;
+
+    T* get(char* arr, size_t entry) {
+        return reinterpret_cast<T*>(&arr[entry * sizeof(T)]);
+    }
+
+    static constexpr uint64_t next_pow2_log(uint64_t capacity) {
+        return capacity <= 16 ? 4 : (64 - __builtin_clzl(capacity - 1));
+    }
+
+    size_t get_free_entry(char* value_used_array, int len, T* value) {
+        size_t entry = m_limiter.limit_input(m_hasher(*value), len);
+
+        // We guarantee that there will always be a place for a new item,
+        // because we regrow it before it is full
+        while (true) {
+            if (value_used_array[entry] == BUCKET_FREE) {
+                return entry;
+            } else {
+                entry++;
+                if (entry == len) {
+                    entry = 0;
+                }
+            }
+        }
+
+        return std::numeric_limits<size_t>::max();
+    }
+
+    void grow_and_rehash(size_t log_new_size) {
+        assert(log_new_size >= m_log_size);
+        size_t new_size = 1 << log_new_size;
+
+        size_t count = 0;
+        char* new_values = nullptr;
+        int result =
+            posix_memalign((void**)&new_values, 64, sizeof(T) * new_size);
+        if (result != 0) {
+            throw std::bad_alloc();
+        }
+        char* new_values_used = (char*)malloc(new_size);
+
+        std::memset(new_values, 0, new_size * sizeof(T));
+        std::memset(new_values_used, 0, new_size);
+
+        for (int i = 0; i < m_size; i++) {
+            if (m_value_used[i] == BUCKET_USED) {
+                size_t entry =
+                    get_free_entry(new_values_used, new_size, get(m_values, i));
+
+                new_values_used[entry] = BUCKET_USED;
+                std::memcpy(&new_values[entry * sizeof(T)],
+                            &m_values[i * sizeof(T)], sizeof(T));
+                count++;
+            }
+        }
+
+        free(m_values);
+        free(m_value_used);
+
+        m_values = new_values;
+        m_value_used = new_values_used;
+        m_size = new_size;
+        m_log_size = log_new_size;
+        m_used = count;
+        m_used_and_deleted = count;
+        m_rehashing_threshhold = m_size * 0.7;
+        m_limiter.set_limit(m_size);
+    }
+};
+
+}  // namespace jsl
---
+++ 2020-10-cachebatchprocessing/hashing_test.cpp
@@ -0,0 +1,128 @@
+
+#include <unordered_set>
+#include "common/argparse.h"
+#include "hash_map.h"
+#include "measure_time.h"
+#include "utils.h"
+
+using namespace jsl;
+using namespace argparse;
+
+enum hash_type_e { SIMPLE, SHIFT };
+
+bool parse_args(int argc,
+                const char* argv[],
+                hash_type_e& out_hash_type,
+                size_t& out_size) {
+    ArgumentParser parser("test123", "123");
+
+    parser.add_argument("-h", "--hash",
+                        "Type of the hash function (simple, shift)", true);
+    parser.add_argument("-s", "--size", "Size of the hash map (s, m, l)", true);
+
+    auto err = parser.parse(argc, argv);
+    if (err) {
+        std::cout << err << std::endl;
+        return false;
+    }
+
+    if (parser.exists("h")) {
+        std::string hash_type = parser.get<std::string>("h");
+        std::cout << "Hash type : " << hash_type << std::endl;
+        if (hash_type == "simple") {
+            out_hash_type = hash_type_e::SIMPLE;
+        } else if (hash_type == "shift") {
+            out_hash_type = hash_type_e::SHIFT;
+        } else {
+            std::cout << "Unknown value for --hash\n";
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    if (parser.exists("s")) {
+        std::string size = parser.get<std::string>("s");
+        std::cout << "Size of the hash map : " << size << std::endl;
+        if (size == "s") {
+            out_size = 16 * 1024;
+        } else if (size == "m") {
+            out_size = 8 * 1024 * 1024;
+        } else if (size == "l") {
+            out_size = 64 * 1024 * 1024;
+        } else {
+            std::cout << "Unknown value for --size\n";
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+template <typename T, typename Q>
+size_t run_test(int size) {
+    int arr_len = size;
+    std::vector<int> v = create_random_array<int>(arr_len, 0, arr_len);
+    hash_map<Q, T> my_map(arr_len);
+    size_t found1 = 0;
+    size_t found2 = 0;
+
+    size_t iterations = 64 * 1024 * 1024 / size;
+    iterations = iterations == 0 ? 1 : iterations;
+
+    int count = 0.7 * size;
+
+    for (int j = 0; j < iterations; j++) {
+        for (size_t i = 0; i < count; i++) {
+            my_map.insert(Q(v[i]));
+        }
+
+        for (size_t i = 0.3 * size; i < size; i++) {
+            my_map.remove(v[i]);
+        }
+
+        {
+            measure_time m("common");
+            std::vector<bool> result(size, false);
+            for (size_t i = 0; i < size; i++) {
+                result[i] = my_map.find(v[i]);
+            }
+            for (size_t i = 0; i < size; i++) {
+                found1 += result[i];
+            }
+        }
+        {
+            measure_time m("parallel 4");
+            std::vector<bool> result = my_map.find(v);
+            for (size_t i = 0; i < size; i++) {
+                found2 += result[i];
+            }
+        }
+    }
+
+    assert(found1 == found2);
+
+    return found1 + found2;
+}
+
+int main(int argc, const char* argv[]) {
+    hash_type_e hash_type;
+    size_t size = 0;
+    size_t found;
+
+    if (!parse_args(argc, argv, hash_type, size)) {
+        return false;
+    }
+
+    if (hash_type == hash_type_e::SIMPLE) {
+        found = run_test<simple_hasher, int>(size);
+    } else if (hash_type == hash_type_e::SHIFT) {
+        found = run_test<shift_hasher, int>(size);
+    }
+
+    std::cout << "Found " << found << std::endl;
+
+    return 0;
+}
