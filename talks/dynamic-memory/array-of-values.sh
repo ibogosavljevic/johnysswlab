4e52b8a85416b17c77cb78f804e51b85fb579242
Ivica Bogosavljevic <ivica@johnysswlab.com>
2020-11-25 20:48:25 +0100

    Add array of values example

---
+++ talks/dynamic-memory/array-of-values.sh
@@ -0,0 +1,6 @@
+make -C ../../2020-08-polymorphism/ clean
+make -C ../../2020-08-polymorphism/
+../../2020-08-polymorphism/polymorphism -a pointer -s 0
+../../2020-08-polymorphism/polymorphism -a pointer -s 1
+../../2020-08-polymorphism/polymorphism -a dod -s 0
+../../2020-08-polymorphism/polymorphism -a dod -s 1
