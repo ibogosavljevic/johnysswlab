8c8d454fa09fc04a8b8a460a024972c7798d0c2f
Ivica Bogosavljevic <ivica@johnysswlab.com>
2022-01-19 18:20:05 +0100

    Add heapsort with ternary heap.

--- 2022-01-sort/heapsort.h
+++ 2022-01-sort/heapsort.h
@@ -33,3 +33,48 @@ void heapsort(std::vector<T>& vec) {
         heapify(vec, i, 0);
     }
 }
+
+
+template <typename T>
+void heapify2(std::vector<T>& vec, int n, int i) {
+    int largest = i;
+
+    int start = 4 * i + 1;
+    int end = std::min(start + 4, n);
+
+    for (int j = start; j < end; j++) {
+        if (vec[j] > vec[largest]) {
+            largest = j;
+        }
+    }
+
+    if (largest != i) {
+        std::swap(vec[i], vec[largest]);
+
+        heapify2(vec, n, largest);
+    }
+}
+
+/*__asm__ (
+    "ucomiss %[vec_j], %[vec_largest];"
+    "cmovbe %[j], %[largest];"
+    : [largest] "+&r"(largest)
+    : [vec_j] "m"(vec_j), [vec_largest] "x"(vec_largest), [j] "r" (j) 
+    : "cc"
+);*/
+
+
+template <typename T>
+void heapsort2(std::vector<T>& vec) {
+
+    int n = vec.size();
+
+    for (int i = n / 4 - 1; i >= 0; i--)
+        heapify2(vec, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        std::swap(vec[0], vec[i]);
+
+        heapify2(vec, i, 0);
+    }
+}
