4efaa72bd077cbe86558d839028675f217a4f0be
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-06-17 15:23:58 +0200

    Hybrid_linked_list measuring two kinds of list.

--- 2022-06-instrucionlevelparallelism/hybrid_linked_list.cpp
+++ 2022-06-instrucionlevelparallelism/hybrid_linked_list.cpp
@@ -2,12 +2,46 @@
 #include <algorithm>
 #include <numeric>
 #include <iostream>
+#include <string>
 
 #include "hybrid_linked_list.h"
 #include "likwid.h"
 
 static constexpr int SIZE = 16 * 1024 * 1024;
 static constexpr int LOOKUP_SIZE = 128;
+
+void run_test(hybrid_linked_list<int>& test_list, std::vector<int>& lookup_values, std::string suffix) {
+    std::string simple_tag = "Layout_Simple_" + suffix;
+    std::string interleaved_tag = "Layout_Interleaved_" + suffix;
+    std::string array_tag = "Layout_Array_" + suffix;
+    std::string index_tag = "Layout_Index_" + suffix;
+
+    LIKWID_MARKER_START(simple_tag.c_str());
+    std::vector<bool> lookup_result_simple = test_list.lookup_values_simple(lookup_values);
+    LIKWID_MARKER_STOP(simple_tag.c_str());
+
+    LIKWID_MARKER_START(interleaved_tag.c_str());
+    std::vector<bool> lookup_result_interleaved = test_list.lookup_values_interleaved(lookup_values);
+    LIKWID_MARKER_STOP(interleaved_tag.c_str());
+
+    LIKWID_MARKER_START(array_tag.c_str());
+    std::vector<bool> lookup_result_array = test_list.lookup_values_array(lookup_values);
+    LIKWID_MARKER_STOP(array_tag.c_str());
+
+    LIKWID_MARKER_START(index_tag.c_str());
+    std::vector<bool> lookup_result_index = test_list.lookup_values_index(lookup_values);
+    LIKWID_MARKER_STOP(index_tag.c_str());
+
+    if (   lookup_result_interleaved == lookup_result_simple && 
+           lookup_result_array == lookup_result_interleaved &&
+           lookup_result_interleaved == lookup_result_index) {
+        std::cout << "Done\n";
+    } else {
+        std::cout << "Result mismatch\n";
+    }
+
+
+}
 
 int main(int argc, char **argv) {
     LIKWID_MARKER_INIT;
@@ -29,31 +63,10 @@ int main(int argc, char **argv) {
         }
     }
 
+    run_test(test_list, lookup_values, "Perfect");
+    
     test_list.shuffle_list(SIZE / 4);
-
-    LIKWID_MARKER_START("Lookup_Simple");
-    std::vector<bool> lookup_result_simple = test_list.lookup_values_simple(lookup_values);
-    LIKWID_MARKER_STOP("Lookup_Simple");
-
-    LIKWID_MARKER_START("Lookup_Interleaved");
-    std::vector<bool> lookup_result_interleaved = test_list.lookup_values_interleaved(lookup_values);
-    LIKWID_MARKER_STOP("Lookup_Interleaved");
-
-    LIKWID_MARKER_START("Lookup_Array");
-    std::vector<bool> lookup_result_array = test_list.lookup_values_array(lookup_values);
-    LIKWID_MARKER_STOP("Lookup_Array");
-
-    LIKWID_MARKER_START("Lookup_Index");
-    std::vector<bool> lookup_result_index = test_list.lookup_values_index(lookup_values);
-    LIKWID_MARKER_STOP("Lookup_Index");
-
-    if (   lookup_result_interleaved == lookup_result_simple && 
-           lookup_result_array == lookup_result_interleaved &&
-           lookup_result_interleaved == lookup_result_index) {
-        std::cout << "Done\n";
-    } else {
-        std::cout << "Result mismatch\n";
-    }
+    run_test(test_list, lookup_values, "Misplaced_25_Percent");
 
     LIKWID_MARKER_CLOSE;
 
