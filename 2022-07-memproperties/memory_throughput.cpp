120a9f8fa3d7c4eeae687664d4928e9965f8a11a
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-06-27 16:43:21 +0200

    Memory throughput experiment update

--- 2022-07-memproperties/memory_throughput.cpp
+++ 2022-07-memproperties/memory_throughput.cpp
@@ -8,7 +8,7 @@
 #include <numeric>
 #include <iostream>
 
-static constexpr int TEST_SIZE = 32 * 1024 * 1024;
+static constexpr int TEST_SIZE = 64 * 1024 * 1024;
 
 
 uint32_t hsum_epi32_avx(__m128i x)
@@ -29,7 +29,7 @@ uint32_t hsum_8x32(__m256i v)
     return hsum_epi32_avx(sum128);
 }
 
-
+template <int X>
 int run_test(std::vector<int>& data, std::vector<int>& indexes, std::string name) {
     assert(data.size() == indexes.size());
     int len = data.size();
@@ -48,6 +48,16 @@ int run_test(std::vector<int>& data, std::vector<int>& indexes, std::string name
     }
 
     LIKWID_MARKER_STOP(name.c_str());
+
+    /*double time = 0.0;
+    int count = 0;
+    int data_volume = (4 * 2 * len);
+    double data_volume_mb = data_volume / (1024 * 1024);
+
+    std::cout << "Region " << name << "\n";
+    std::cout << "Calculated data volume = " << data_vol    ume / (1024 * 1024) << " MB\n";
+    LIKWID_MARKER_GET(name.c_str(), nullptr, nullptr, &time, &count);
+    std::cout << "Calculated throughput = " << data_volume_mb / time << " MB/s\n";*/
 
     return hsum_8x32(sum);
 }
@@ -78,24 +88,32 @@ int main(int argc, char** argv) {
 
     std::vector<int> indexes(TEST_SIZE);
     std::iota(indexes.begin(), indexes.end(), 0);
-    int sum1 = run_test(data, indexes, "Sequential");
+    int sum1 = run_test<1>(data, indexes, "Sequential");
     std::cout << sum1 << std::endl;
 
     std::random_shuffle(indexes.begin(), indexes.end());
-    int sum2 = run_test(data, indexes, "Random");
+    int sum2 = run_test<2>(data, indexes, "Random");
     std::cout << sum2 << std::endl;
 
     generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 4);
-    int sum3 = run_test(data, indexes, "Stride_4");
+    int sum3 = run_test<3>(data, indexes, "Stride_4");
     std::cout << sum3 << std::endl;
 
     generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 16);
-    int sum4 = run_test(data, indexes, "Stride_16");
+    int sum4 = run_test<4>(data, indexes, "Stride_16");
     std::cout << sum4 << std::endl;
 
-    generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 64);
-    int sum5 = run_test(data, indexes, "Stride_64");
+    generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 32);
+    int sum5 = run_test<5>(data, indexes, "Stride_32");
     std::cout << sum5 << std::endl;
+
+    generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 64);
+    int sum6 = run_test<6>(data, indexes, "Stride_64");
+    std::cout << sum6 << std::endl;
+
+    generate_random_index_vector(indexes.begin(), indexes.end(), TEST_SIZE, 128);
+    int sum7 = run_test<7>(data, indexes, "Stride_128");
+    std::cout << sum7 << std::endl;
 
     LIKWID_MARKER_CLOSE;
 
