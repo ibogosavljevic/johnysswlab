7b3bb6587e2efb88fe89f5d0df6404c073eecb48
Ivica Bogosavljevic <ivica@johnysswlab.com>
2022-07-06 22:47:15 +0200

    Add -mavx2 flag to compile script

--- 2022-07-memproperties/compile.sh
+++ 2022-07-memproperties/compile.sh
@@ -1,2 +1,5 @@
-clang++ -O3 -mavx -fopenmp-simd -DLIKWID_PERFMON cache_line.cpp -o cache_line -llikwid
-clang++ -O3 -mavx -fopenmp-simd -DLIKWID_PERFMON arithmetic_intensity.cpp -o arithmetic_intensity -llikwid
+CC="clang++ -O3 -mavx2 -fopenmp-simd -DLIKWID_PERFMON -llikwid"
+
+$CC memory_throughput.cpp -o memory_throughput
+$CC cache_line.cpp -o cache_line
+$CC arithmetic_intensity.cpp -o arithmetic_intensity
