baee93d40ba5a0ae6db00655cd78a5fe90884757
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-07-07 07:09:34 +0000

    Rewrite compile-arm.sh script

--- 2022-07-memproperties/compile-arm.sh
+++ 2022-07-memproperties/compile-arm.sh
@@ -1,2 +1,5 @@
-clang++ -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID_PERFMON cache_line.cpp -o cache_line -llikwid
-clang++ -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID_PERFMON arithmetic_intensity.cpp -o arithmetic_intensity -llikwid
+CC="clang++ -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID_PERFMON -llikwid"
+
+$CC memory_throughput.cpp -o memory_throughput
+$CC cache_line.cpp -o cache_line
+$CC arithmetic_intensity.cpp -o arithmetic_intensity
