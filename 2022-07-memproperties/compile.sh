7fbd7df8bbc702b544a9df38e384216fed0a093d
Ivica Bogosavljevic <ivica@johnysswlab.com>
2022-07-08 13:44:32 +0200

    Add read vs write example

--- 2022-07-memproperties/compile.sh
+++ 2022-07-memproperties/compile.sh
@@ -1,5 +1,6 @@
-CC="clang++ -O3 -mavx2 -fopenmp-simd -DLIKWID_PERFMON -llikwid"
+CC="clang++ -g -O3 -mavx2 -fopenmp-simd -DLIKWID_PERFMON -llikwid"
 
+$CC read_vs_write.cpp -o read_vs_write
 $CC memory_throughput.cpp -o memory_throughput
 $CC cache_line.cpp -o cache_line
 $CC arithmetic_intensity.cpp -o arithmetic_intensity
--- 2022-07-memproperties/read_vs_write.cpp
+++ 2022-07-memproperties/read_vs_write.cpp
@@ -1,41 +1,115 @@
 #include "likwid.h"
 #include <string>
 #include <cstdlib>
+#include <iostream>
 
 template <typename T>
-void rotate_matrix_write(T* out, T* in, int n) {
+void rotate_matrix_write(T* out, T* in, int n, int unused_count) {
+    int u = unused_count;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            out[i * n + j] = in[j * n + i];
+            out[(i + u) * n + j] = in[(j + u) * n + i];
         }
     }
 }
 
 template <typename T>
-void rotate_matrix_read(T* out, T* in, int n) {
+void rotate_matrix_read(T* out, T* in, int n, int unused_count) {
+    int u = unused_count;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            out[j * n + i] = in[i * n + j];
+            out[(j + u) * n + i] = in[(i + u) * n + j];
         }
     }
 }
 
 template <typename T>
-void fill_matrix(T* out, int n) {
+void rotate_matrix_write_nontemporal(T* out, T* in, int n, int unused_count) {
+    int u = unused_count;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            out[j * n + i] = i * n + j;
+            __builtin_nontemporal_store(in[(j + u) * n + i], out + (i + u) * n + j);
         }
     }
 }
 
-int MATRIX_SIZE = 1024 + 128;
+template <typename T>
+void fill_matrix(T* out, int n, int unused_count) {
+    int u = unused_count;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            out[(j + u) * n + i] = i * n + j;
+        }
+    }
+}
+
+template <typename T>
+T* allocate_matrix(int n, int unused_count) {
+    return (T*) malloc(n * (n + unused_count) * sizeof(T));
+}
+
+template <typename T>
+void free_matrix(T* t) {
+    free(t);
+}
+
+static constexpr int UNUSED_COUNT = 12;
+
+std::string get_test_suffix(int n, int repeat_count) {
+    return "_" + std::to_string(n) + "_" + std::to_string(repeat_count);
+}
+
+static void escape(void* p) {
+    asm volatile("" : : "g"(p) : "memory");
+}
 
 int main(int argc, char** argv) {
     LIKWID_MARKER_INIT;
 
-    double* in1 = malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
-    double* in2 = malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(double));
+    int start_n = 32;
+    int end_n = 8 * 1024;
+    
+    for (int n = start_n; n <= end_n; n *= 2) {
+        std::cout << "N = " << n << std::endl;
+        int repeat_count = (end_n / n) * 16;
+        
+        int* in_matrix = allocate_matrix<int>(n, UNUSED_COUNT);
+        int* out_matrix1 = allocate_matrix<int>(n, UNUSED_COUNT);
+        int* out_matrix2 = allocate_matrix<int>(n, UNUSED_COUNT);
+        int* out_matrix3 = allocate_matrix<int>(n, UNUSED_COUNT);
+
+        std::string read_name = "READ" + get_test_suffix(n, repeat_count);
+        std::string write_name = "WRITE" + get_test_suffix(n, repeat_count);
+        std::string write_nontemporal_name = "WRITE_NONTEMPORAL" + get_test_suffix(n, repeat_count);
+        fill_matrix<int>(in_matrix, n, UNUSED_COUNT);
+
+        LIKWID_MARKER_START(read_name.c_str());
+        for (int i = 0; i < repeat_count; i++) {
+            rotate_matrix_read(out_matrix1, in_matrix, n, UNUSED_COUNT);
+            escape(out_matrix1);
+        }
+        LIKWID_MARKER_STOP(read_name.c_str());
+
+        LIKWID_MARKER_START(write_name.c_str());
+        for (int i = 0; i < repeat_count; i++) {
+            rotate_matrix_write(out_matrix2, in_matrix, n, UNUSED_COUNT);
+            escape(out_matrix2);
+        }
+        LIKWID_MARKER_STOP(write_name.c_str());
+
+        LIKWID_MARKER_START(write_nontemporal_name.c_str());
+        for (int i = 0; i < repeat_count; i++) {
+            rotate_matrix_write_nontemporal(out_matrix3, in_matrix, n, UNUSED_COUNT);
+            escape(out_matrix3);
+        }
+        LIKWID_MARKER_STOP(write_nontemporal_name.c_str());
+
+        free_matrix(in_matrix);
+        free_matrix(out_matrix1);
+        free_matrix(out_matrix2);
+        free_matrix(out_matrix3);
+    }
+
 
     LIKWID_MARKER_CLOSE;
 }
