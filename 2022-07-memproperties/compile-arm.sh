98a5c13e46b3f629b3fd884bc2387b461f433f76
Ivica Bogosavljevic <ivica@johnysswlab.com>
2022-07-12 17:44:06 +0200

    Rename arithmetic_intensity.cpp to sequential_test.cpp

--- 2022-07-memproperties/compile-arm.sh
+++ 2022-07-memproperties/compile-arm.sh
@@ -2,4 +2,4 @@ CC="clang++ -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID_PERFMON -l
 
 $CC memory_throughput.cpp -o memory_throughput
 $CC cache_line.cpp -o cache_line
-$CC arithmetic_intensity.cpp -o arithmetic_intensity
+$CC sequential_test.cpp -o sequential_test
--- 2022-07-memproperties/compile.sh
+++ 2022-07-memproperties/compile.sh
@@ -3,4 +3,4 @@ CC="clang++ -g -O3 -mavx2 -fopenmp-simd -DLIKWID_PERFMON -llikwid"
 $CC read_vs_write.cpp -o read_vs_write
 $CC memory_throughput.cpp -o memory_throughput
 $CC cache_line.cpp -o cache_line
-$CC arithmetic_intensity.cpp -o arithmetic_intensity
+$CC sequential_test.cpp -o sequential_test
