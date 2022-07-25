4237ef81541099f209c5b562550ee9b52b64f889
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-07-25 05:45:07 +0000

    Add read_vs_write to compile-arm.sh

--- 2022-07-memproperties/compile-arm.sh
+++ 2022-07-memproperties/compile-arm.sh
@@ -1,5 +1,6 @@
 CC="clang++ -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID_PERFMON -llikwid"
 
+$CC read_vs_write.cpp -o read_vs_write
 $CC memory_throughput.cpp -o memory_throughput
 $CC cache_line.cpp -o cache_line
 $CC sequential_test.cpp -o sequential_test
