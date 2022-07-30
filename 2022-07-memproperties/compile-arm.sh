01795ad8988fe5ce5db68d85886e942937f205eb
Ivica Bogosavljevic <ivica@johnysswlab.com>
2022-07-30 12:37:06 +0200

    Moving cache line example to 2022-08-memproperties2

--- 2022-07-memproperties/compile-arm.sh
+++ 2022-07-memproperties/compile-arm.sh
@@ -3,5 +3,4 @@ CC="clang++ -std=c++17 -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp-simd -DLIKWID
 $CC arithmetic_intensity.cpp -o arithmetic_intensity
 $CC read_vs_write.cpp -o read_vs_write
 $CC memory_throughput.cpp -o memory_throughput
-$CC cache_line.cpp -o cache_line
 $CC sequential_test.cpp -o sequential_test
--- 2022-07-memproperties/compile.sh
+++ 2022-07-memproperties/compile.sh
@@ -3,5 +3,4 @@ CC="clang++ -std=c++17 -g -O3 -mavx2 -fopenmp-simd -DLIKWID_PERFMON -llikwid"
 $CC arithmetic_intensity.cpp -o arithmetic_intensity
 $CC read_vs_write.cpp -o read_vs_write
 $CC memory_throughput.cpp -o memory_throughput
-$CC cache_line.cpp -o cache_line
 $CC sequential_test.cpp -o sequential_test
