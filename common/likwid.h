44d8eebae0793b95668f82bffcc59cf78895a809
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-09-10 23:37:44 +0200

    Add multithreading support to likwid.

--- common/likwid.h
+++ common/likwid.h
@@ -7,6 +7,8 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include "omp.h"
+
 
 template <typename UNUSED>
 class likwid_stub {
@@ -15,13 +17,21 @@ public:
     void close() {
         struct compare_obj {
             bool operator() (const measure_data_t& lhs, const measure_data_t& rhs) const { return lhs.created < rhs.created; }
-	};
+	    };
 
         std::vector<std::pair<std::string, measure_data_t>> m_measure_data_sorted(m_measure_data.begin(), m_measure_data.end());
-	std::sort(m_measure_data_sorted.begin(), m_measure_data_sorted.end(), [](const auto& lhs, const auto& rhs) -> bool { return lhs.second.created < rhs.second.created; }); 
+	    std::sort(m_measure_data_sorted.begin(), m_measure_data_sorted.end(), [](const auto& lhs, const auto& rhs) -> bool { return lhs.second.created < rhs.second.created; }); 
 
-        for (auto d: m_measure_data_sorted) {
-            std::cout << "Region: " << d.first << ", count " << d.second.total_count << ", total runtime " << timespec_tosec(&d.second.total_time) << std::endl;
+        for (std::pair<std::string, measure_data_t> d: m_measure_data_sorted) {
+            std::string& region_name = d.first;
+
+            int id = 0;
+            std::cout << "Region " << region_name << "\n";
+            for (const auto & m: d.second.thread_data) {
+                std::cout << "\tCPU id = " << id << ", count = " << m.second.total_count << ", runtime " << timespec_tosec(&m.second.total_time) << "\n";
+                id++;
+            }
+
         }
     }
 
@@ -29,16 +39,23 @@ public:
         std::string str_reg_name(region_name);
         measure_data_t* my_datum = &m_measure_data[str_reg_name];
 
-	if (my_datum->created == -1) {
+        if (my_datum->created == -1) {
             my_datum->created = m_created_count;
-	    m_created_count++;
-	}
+            m_created_count++;
+        }
 
-        if (my_datum->started_time.tv_sec != 0 || my_datum->started_time.tv_nsec != 0) {
+        cpu_id_t cpu_id = get_current_cpu();
+        /*cpu_affinity_mask_t affinity_mask = get_affinity_mask();
+        set_affinity_mask(cpu_id);
+        yield();*/
+
+        typename measure_data_t::thread_measure_data_t& thread_datum = my_datum->thread_data[cpu_id];
+
+        if (thread_datum.started_time.tv_sec != 0 || thread_datum.started_time.tv_nsec != 0) {
             std::cout << "Region " << str_reg_name << " already started. Restarting\n";
         }
 
-        clock_gettime(CLOCK_MONOTONIC, &my_datum->started_time);
+        clock_gettime(CLOCK_MONOTONIC, &thread_datum.started_time);
     }
 
     void stop(const char * region_name) {
@@ -50,7 +67,11 @@ public:
             return;
         }
 
-        if (datum->second.started_time.tv_sec == 0 && datum->second.started_time.tv_sec == 0) {
+        cpu_id_t cpu_id = get_current_cpu();
+
+        typename measure_data_t::thread_measure_data_t& thread_datum = datum->second.thread_data[cpu_id];
+
+        if (thread_datum.started_time.tv_sec == 0 && thread_datum.started_time.tv_sec == 0) {
             std::cout << "Region " << str_reg_name << " not started\n";
             return;
         }
@@ -58,12 +79,14 @@ public:
         struct timespec finish, runtime;
         clock_gettime(CLOCK_MONOTONIC, &finish);
 
-        timespec_sub(&runtime, &finish, &datum->second.started_time);
-        timespec_add(&datum->second.total_time, &datum->second.total_time, &runtime);
+        timespec_sub(&runtime, &finish, &thread_datum.started_time);
+        timespec_add(&thread_datum.total_time, &thread_datum.total_time, &runtime);
 
-        datum->second.total_count++;
-        datum->second.started_time.tv_sec = 0;
-        datum->second.started_time.tv_nsec = 0;
+        thread_datum.total_count++;
+        thread_datum.started_time.tv_sec = 0;
+        thread_datum.started_time.tv_nsec = 0;
+
+        
     }
 
     static likwid_stub& get_instance() {
@@ -72,13 +95,21 @@ public:
     }
 
 private:
-    struct measure_data_t {
-        int total_count;
-        struct timespec total_time;
-        struct timespec started_time;
-	int created;
+    using cpu_id_t = int;
+    using cpu_affinity_mask_t = cpu_set_t;
 
-        measure_data_t() : total_count(0), total_time{0,0}, started_time{0, 0}, created(-1) {}
+    struct measure_data_t {
+        struct thread_measure_data_t {
+            int total_count;
+            struct timespec total_time;
+            struct timespec started_time;
+
+            thread_measure_data_t() : total_count(0), total_time{0,0}, started_time{0, 0} {}
+        };
+    	int created;
+        std::unordered_map<cpu_id_t, thread_measure_data_t> thread_data;
+
+        measure_data_t() : created(-1) {}
 
 	bool operator()(const measure_data_t& lhs, const measure_data_t& rhs) const { lhs.created < rhs.created; } 
     };
@@ -105,11 +136,15 @@ private:
         }
     }
 
-    static double timespec_tosec(struct timespec* diff) {
+    static double timespec_tosec(const struct timespec* diff) {
         double seconds;
 
         seconds = diff->tv_sec + (diff->tv_nsec / 1000000000.0);
         return seconds;
+    }
+
+    static cpu_id_t get_current_cpu() {
+        return omp_get_thread_num();
     }
 
 
