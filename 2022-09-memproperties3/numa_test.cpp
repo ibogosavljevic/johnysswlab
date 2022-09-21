f8a2f13cc460b4e8471de3fbcdac7196d713f20d
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-09-21 11:13:50 +0200

    Numa test fix array size

--- 2022-09-memproperties3/numa_test.cpp
+++ 2022-09-memproperties3/numa_test.cpp
@@ -8,42 +8,42 @@ static void clobber() {
   asm volatile("" : : : "memory");
 }
 
-void initialize_data_single_threaded(std::vector<double>& values, int size) {
+void initialize_data_single_threaded(std::vector<double>& values, size_t size) {
     values.resize(size);
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         values[i] = 1.0 / static_cast<double>(i);
     }
 }
 
 void copy_data_multithreaded_static(std::vector<double>&dst, const std::vector<double>&src, int num_cores) {
-    int size = src.size();
+    size_t size = src.size();
     dst.resize(size);
 
     double * dst_ptr = &dst[0];
     double const * src_ptr = &src[0];
 
     #pragma omp parallel for default(none) shared(src_ptr, dst_ptr, size) schedule(static) num_threads(num_cores)
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         dst_ptr[i] = src_ptr[i];
     }
 }
 
 void copy_data_multithreaded_random(std::vector<double>&dst, const std::vector<double>&src, int num_cores) {
-    int size = src.size();
+    size_t size = src.size();
     dst.resize(size);
 
     double * dst_ptr = &dst[0];
     double const * src_ptr = &src[0];
 
     static constexpr int PAGE_SIZE = 4 * 1024 / sizeof(double);
-    int current_index = 0;
+    size_t current_index = 0;
 
     #pragma omp parallel default(none) shared(src_ptr, dst_ptr, size, current_index) num_threads(num_cores)
     {
         srand(int(time(NULL)) ^ omp_get_thread_num());
 
         while(true) {
-            int start;
+            size_t start;
             #pragma omp critical
             {
                 start = current_index;
@@ -54,11 +54,11 @@ void copy_data_multithreaded_random(std::vector<double>&dst, const std::vector<d
                 break;
             }
 
-            int end = std::min(start + PAGE_SIZE, size);
+            size_t end = std::min(start + PAGE_SIZE, size);
             int repeat_count = rand() % 3 + 1;
 
             for (int r = 0; r < repeat_count; r++) {
-                for (int i = start; i < end; i++) {
+                for (size_t i = start; i < end; i++) {
                     dst_ptr[i] = src_ptr[i];
                 }
                 clobber();
@@ -69,7 +69,7 @@ void copy_data_multithreaded_random(std::vector<double>&dst, const std::vector<d
 
 
 double __attribute__ ((noinline)) run_test(const std::vector<double> v, int num_threads, std::string name) {
-    int size = v.size();
+    size_t size = v.size();
     const double* v_ptr = &v[0];
 
     double sum = 0;
@@ -77,7 +77,7 @@ double __attribute__ ((noinline)) run_test(const std::vector<double> v, int num_
     LIKWID_MARKER_START(name.c_str());
 
     #pragma omp parallel for default(none) shared(v_ptr, size) reduction(+:sum) schedule(static) num_threads(num_threads)
-    for (int i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++) {
         sum += v_ptr[i];
     }
 
@@ -88,13 +88,14 @@ double __attribute__ ((noinline)) run_test(const std::vector<double> v, int num_
 
 int main(int argc, char **argv) {
 
-    int SIZE = 128 * 1024 * 1024;
+    size_t SIZE = 1l * 1024l * 1024l * 1024l;
     std::vector<double> data_single;
     std::vector<double> data_multi_static;
     std::vector<double> data_multi_random;
     double r;
 
     int num_threads = omp_get_max_threads();
+    std::cout << "Num threads " << num_threads << "\n";
 
     LIKWID_MARKER_INIT;
 
