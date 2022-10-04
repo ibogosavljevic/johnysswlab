7cb7d7196d5078c3d2b0e1ec659ab820125a6a2a
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-10-04 16:15:11 +0200

    Add linear search to compile-arm.sh

--- 2022-10-memproperties3/compile-arm.sh
+++ 2022-10-memproperties3/compile-arm.sh
@@ -1,5 +1,6 @@
 CC="clang++ -std=c++17 -g -O3 -mcpu=cortex-a72 -mfpu=neon -fopenmp -DLIKWID_PERFMON -llikwid"
 
+$CC linear_search_multithreading.cpp -o linear_search_multithreading
 $CC binary_search_multithreading.cpp -o binary_search_multithreading
 $CC memory_throughput.cpp -o memory_throughput
 $CC memory_throughput_independent.cpp -o memory_throughput_independent
