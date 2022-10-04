b9cefd4867a64a97021ae47b0122fc337e1d8b19
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-10-04 11:41:59 +0200

    Add linear multithreading example

--- 2022-10-memproperties3/compile.sh
+++ 2022-10-memproperties3/compile.sh
@@ -2,6 +2,7 @@ CC="clang++ -std=c++17 -g -O3 -mavx2 -fopenmp -DLIKWID_PERFMON -llikwid"
 
 #$CC arithmetic_intensity.cpp -o arithmetic_intensity
 #$CC read_vs_write.cpp -o read_vs_write
+$CC linear_search_multithreading.cpp -o linear_search_multithreading
 $CC numa_test.cpp -o numa_test
 $CC binary_search_multithreading.cpp -o binary_search_multithreading
 $CC memory_throughput.cpp -o memory_throughput
