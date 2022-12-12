590fe7373eb0474c5486e3d92827538c3acfc29b
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2022-12-12 19:03:21 +0100

    Add CLANG's optimization report

--- 2022-12-optimizationreport/compile.sh
+++ 2022-12-optimizationreport/compile.sh
@@ -1,3 +1,3 @@
-clang++ -O3 -fsave-optimization-record compiler-optimizations.cpp -o compiler-optimizations -c -Rpass-missed=.* -Rpass=.* -Rpass-analysis=.* 2>optimizations.txt
+clang++ -O3 -fsave-optimization-record compiler-optimizations.cpp -o compiler-optimizations -c -Rpass-missed=.* -Rpass=.* -Rpass-analysis=.* 2>optimizations-clang.txt
 python3 /usr/lib/llvm-12/share/opt-viewer/opt-viewer.py compiler-optimizations.opt.yaml
 open html/index.html
