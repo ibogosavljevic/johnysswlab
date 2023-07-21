171071f3280808a7b8af4856344bf3f10a2349cf
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2023-07-21 17:16:25 +0200

    Cache conflict compile.sh - disable identical code folding

--- 2023-02-cache-conflicts/compile.sh
+++ 2023-02-cache-conflicts/compile.sh
@@ -1,1 +1,1 @@
-clang -O3 -g -mavx2 cache_conflicts.c -DLIKWID_PERFMON -o cache_conflicts -llikwid
+clang -O3 -g -mavx2 -fuse-ld=lld -Wl,--icf=none cache_conflicts.c -DLIKWID_PERFMON -o cache_conflicts -llikwid
