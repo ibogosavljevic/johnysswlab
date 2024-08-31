c261f64151a47da0c111154c6c1ad22d35941784
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2024-08-31 21:14:02 +0200

    Update convergence loop with additional example

--- 2024-08-convergence-loop/convergence_loop.cpp
+++ 2024-08-convergence-loop/convergence_loop.cpp
@@ -78,6 +78,39 @@ result_t calculate_pi_vector1(double convergence_diff) {
     return { reduce(sum), tmp[3] };
 }
 
+result_t calculate_pi_vector3(double convergence_diff) {
+    __m256d sum = _mm256_set1_pd(0.0);
+
+    const __m256d convergence_diff_v = _mm256_set1_pd(convergence_diff);
+
+    __m256d i1 = _mm256_setr_pd(1.0, 5.0, 9.0, 13.0);
+    __m256d i2 = _mm256_setr_pd(3.0, 7.0, 11.0, 15.0);
+    const __m256d one = _mm256_set1_pd(1.0);
+    const __m256d one_minus = _mm256_set1_pd(-1.0);
+    const __m256d inc = _mm256_set1_pd(16.0);
+
+    const __m256d clean_last_lane = _mm256_castsi256_pd(_mm256_setr_epi64x(-1, -1, -1, 0));
+
+    alignas(32) double tmp[4];
+    __m256d x{};
+
+    do {
+        __m256d x1 = _mm256_div_pd(one, i1);
+        __m256d x2 = _mm256_div_pd(one_minus, i2);
+
+        i1 = _mm256_add_pd(i1, inc);
+        i2 = _mm256_add_pd(i2, inc);
+
+        x = _mm256_add_pd(x1, x2);
+        sum = _mm256_add_pd(sum, x);
+
+    } while (_mm256_movemask_pd(_mm256_cmp_pd(x, convergence_diff_v, _CMP_LE_OS)) == 0);
+
+    _mm256_store_pd(tmp, i2);
+
+    return { reduce(sum), tmp[3] };
+}
+
 
 result_t calculate_pi_vector2(double convergence_diff) {
     __m256d sum = _mm256_set1_pd(0.0);
@@ -129,17 +162,21 @@ int main(int argc, char** argv) {
     result_t pi_compiler = calculate_pi(0.0);
     LIKWID_MARKER_STOP("compiler_pi");
 
-    LIKWID_MARKER_START("vector_pi1");
-    result_t pi_vector1 = calculate_pi_vector1(1e-18);
-    LIKWID_MARKER_STOP("vector_pi1");
+    LIKWID_MARKER_START("vector3_pi1");
+    result_t pi_vector31 = calculate_pi_vector3(1e-18);
+    LIKWID_MARKER_STOP("vector3_pi1");
 
-    LIKWID_MARKER_START("vector_pi2");
-    result_t pi_vector2 = calculate_pi_vector1(1e-17);
-    LIKWID_MARKER_STOP("vector_pi2");
+    LIKWID_MARKER_START("vector3_pi2");
+    result_t pi_vector32 = calculate_pi_vector3(1e-17);
+    LIKWID_MARKER_STOP("vector3_pi2");
 
-    LIKWID_MARKER_START("vector_pi3");
-    result_t pi_vector3 = calculate_pi_vector1(4e-17);
-    LIKWID_MARKER_STOP("vector_pi3");
+    LIKWID_MARKER_START("vector3_pi3");
+    result_t pi_vector33 = calculate_pi_vector3(4e-17);
+    LIKWID_MARKER_STOP("vector3_pi3");
+
+    LIKWID_MARKER_START("vector1_pi1");
+    result_t pi_vector11 = calculate_pi_vector1(4e-17);
+    LIKWID_MARKER_STOP("vector1_pi1");
 
     LIKWID_MARKER_START("vector2_pi1");
     result_t pi_vector21 = calculate_pi_vector2(4e-17);
@@ -147,17 +184,18 @@ int main(int argc, char** argv) {
 
     std::cout << std::setprecision (std::numeric_limits<double>::digits10 + 1);
     std::cout << "compiler diff " << std::abs(pi4 - pi_compiler.pi_4) << "\n";
-    std::cout << "vector1 diff " << std::abs(pi4 - pi_vector1.pi_4) << "\n";
-    std::cout << "vector2 diff " << std::abs(pi4 - pi_vector2.pi_4) << "\n";
-    std::cout << "vector3 diff " << std::abs(pi4 - pi_vector3.pi_4) << "\n";
-    std::cout << "vector2x diff " << std::abs(pi4 - pi_vector21.pi_4) << "\n";
+    std::cout << "vector3_1 diff " << std::abs(pi4 - pi_vector31.pi_4) << "\n";
+    std::cout << "vector3_2 diff " << std::abs(pi4 - pi_vector32.pi_4) << "\n";
+    std::cout << "vector3_3 diff " << std::abs(pi4 - pi_vector33.pi_4) << "\n";
+    std::cout << "vector1 diff " << std::abs(pi4 - pi_vector11.pi_4) << "\n";
+    std::cout << "vector2 diff " << std::abs(pi4 - pi_vector21.pi_4) << "\n";
 
     std::cout << "compiler last member " << pi_compiler.last_member << "\n";
-    std::cout << "vector1 last member " << pi_vector1.last_member << "\n";
-    std::cout << "vector2 last member " << pi_vector2.last_member << "\n";
-    std::cout << "vector3 last member " << pi_vector3.last_member << "\n";
-    std::cout << "vector2x last member " << pi_vector21.last_member << "\n";
-
+    std::cout << "vector3_1 last member " << pi_vector31.last_member << "\n";
+    std::cout << "vector3_2 last member " << pi_vector32.last_member << "\n";
+    std::cout << "vector3_3 last member " << pi_vector33.last_member << "\n";
+    std::cout << "vector1 last member " << pi_vector11.last_member << "\n";
+    std::cout << "vector2 last member " << pi_vector21.last_member << "\n";
 
     LIKWID_MARKER_CLOSE;
 
