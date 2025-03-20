ee724fb5e8cfc4fbbc0102cb8769d47078f0ec84
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2025-03-20 16:44:33 +0100

    Add resize memory example

---
+++ 2025-03-resize-memory/vector.h
@@ -0,0 +1,129 @@
+#include <cstdlib>
+#include <stdexcept>
+#include <initializer_list>
+#include <iostream>
+#include <sys/mman.h>
+
+namespace jsl {
+
+    class simple_allocator {
+    public:
+        static void* alloc(std::size_t size) {
+            return std::malloc(size);
+        }
+
+        static bool resize(void* ptr, std::size_t old_size, std::size_t new_size) {
+            return false;
+        }
+
+        static void free(void* ptr, std::size_t size) {
+            std::free(ptr);
+        }
+    };
+
+    class resize_allocator {
+    public:
+        static void* alloc(std::size_t size) {
+            std::cout << "Allocate size " << size << "\n";
+            void * res = mmap(NULL, size, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+            if (res == MAP_FAILED) {
+                return nullptr;
+            } else {
+                return res;
+            }
+        }
+
+        static bool resize(void* ptr, std::size_t old_size, std::size_t new_size) {
+            void * res = mremap(ptr, old_size, new_size, 0);
+            std::cout << "Resize, old size " << old_size << ", new size " << new_size << ", resize successfull " << (res != MAP_FAILED) << "\n" ;
+            return res != MAP_FAILED;
+        }
+
+        static void free(void* ptr, std::size_t size) {
+            std::cout << "Free size " << size << "\n";
+            munmap(ptr, size);
+        }
+    };
+
+    template <typename T, typename Allocator>
+    class vector {
+    private:
+        T* data;
+        std::size_t sz;
+        std::size_t cap;
+
+        void resize_capacity(std::size_t new_cap) {
+            if (!data) {
+                data = reinterpret_cast<T*>(Allocator::alloc(new_cap * sizeof(T)));
+                if (!data) {
+                    throw std::bad_alloc();
+                }
+            } else {
+                bool resize_succ = Allocator::resize(data, cap * sizeof(T), new_cap * sizeof(T));
+
+                if (!resize_succ) {
+                    T* new_data = reinterpret_cast<T*>(Allocator::alloc(new_cap * sizeof(T)));
+                    if (!new_data) {
+                        throw std::bad_alloc();
+                    }
+
+                    for (std::size_t i = 0; i < sz; ++i) {
+                        new (&new_data[i]) T(std::move(data[i]));
+                        data[i].~T();
+                    }
+
+                    Allocator::free(data, cap);
+                    data = new_data;
+                }
+            }
+
+            cap = new_cap;
+        }
+    
+    public:
+        vector() : data(nullptr), sz(0), cap(0) {}
+
+        vector(std::initializer_list<T> init) : vector() {
+            reserve(init.size());
+            for (const auto& val : init) push_back(val);
+        }
+
+        ~vector() {
+            clear();
+            if (data) {
+                Allocator::free(data, cap);
+            }
+        }
+
+        void push_back(const T& value) {
+            if (sz == cap) resize_capacity(cap ? cap * 2 : 1);
+            new (&data[sz++]) T(value);
+        }
+
+        void pop_back() {
+            if (sz == 0) throw std::out_of_range("pop_back() on empty vector");
+            data[--sz].~T();
+        }
+
+        T& operator[](std::size_t index) {
+            return data[index];
+        }
+
+        const T& operator[](std::size_t index) const {
+            return data[index];
+        }
+
+        std::size_t size() const { return sz; }
+
+        void clear() {
+            for (std::size_t i = 0; i < sz; ++i) data[i].~T();
+            sz = 0;
+        }
+
+        void reserve(std::size_t new_cap) {
+            if (new_cap > cap) resize_capacity(new_cap);
+        }
+    };
+}
---
+++ 2025-03-resize-memory/vector_test.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <iostream>
+#include <random>
+#include "likwid.h"
+#include "vector.h"
+
+std::vector<float> generate_random_vector(std::size_t size) {
+    std::vector<float> vec;
+    vec.reserve(size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+    
+    for (std::size_t i = 0; i < size; ++i) {
+        vec.push_back(dis(gen));
+    }
+    return vec;
+}
+
+template<typename Vec>
+void run_test(Vec& v, const std::vector<float> & input, const char * name) {
+    LIKWID_MARKER_START(name);
+    for (size_t i = 0; i < input.size(); i++) {
+        v.push_back(input[i]);
+    }
+    LIKWID_MARKER_STOP(name);
+}
+
+template<typename Vec0, typename Vec1>
+void compare_vectors(const Vec0& v0, const Vec1& v1) {
+    if (v0.size() != v1.size()) {
+        std::cout << "Size not same\n";
+        return;
+    }
+
+    for (size_t i = 0; i < v0.size(); i++) {
+        if (v0[i] != v1[i]) {
+            std::cout << "Difference at " << i << "\n";
+            return;
+        }
+    }
+
+    std::cout << "Same\n";
+}
+
+int main() {
+    std::vector<float> std_vector, input_vector = generate_random_vector(32*1024*1024);
+    jsl::vector<float, jsl::simple_allocator> jsl_vector;
+    jsl::vector<float, jsl::resize_allocator> jsl_resize_vector;
+
+    LIKWID_MARKER_INIT;
+
+    run_test(std_vector, input_vector, "std_vector");
+    run_test(jsl_vector, input_vector, "jsl_vector");
+    run_test(jsl_resize_vector, input_vector, "jsl_resize_vector");
+
+    compare_vectors(std_vector, input_vector);
+    compare_vectors(jsl_vector, input_vector);
+    compare_vectors(jsl_resize_vector, input_vector);
+
+    LIKWID_MARKER_CLOSE;
+}
